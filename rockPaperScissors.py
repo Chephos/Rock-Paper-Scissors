66a2573ee8fe594c6698e481c08e1de2bd662c3c
Chephos <efosacharlesabu@gmail.com>
2022-05-06 15:08:57 +0100

    Added some spaces and editted capitalizations

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -2,16 +2,21 @@ import random
 
 options=['rock','paper','scissors']
 print("Instruction: Type in 'q' to quit.")
+
 while True:
     user_input = input('Rock/Paper/Scissors: ').lower()
+
     if user_input=='q':
         break
+
     if user_input not in options:
         print('Your input is not valid.')
         continue
+
     rand=random.randint(0,2)
     computer_pick= options[rand]
     print(f"Computer picked {computer_pick}.")
+
     if user_input=='scissors' and computer_pick== 'paper':
         print('You win!')
     elif user_input=='rock' and computer_pick=='scissors':
@@ -19,6 +24,6 @@ while True:
     elif user_input=='paper' and computer_pick=='rock':
         print('You win!')    
     elif user_input==computer_pick:
-        print("it's a tie!!")
+        print("It's a tie!!")
     else:
         print('Otu, you lost!!')
